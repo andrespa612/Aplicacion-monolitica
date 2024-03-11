1ea1f6bfe2efc2d3a499a4c8687456068182ea34
andrespa612 <38359603+andrespa612@users.noreply.github.com>
2024-03-11 12:25:07 -0600

    App creada

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+# Lista de tareas
+tasks = []
+
+# Ruta principal para mostrar la lista de tareas
+@app.route('/')
+def index():
+    return render_template('index.html', tasks=tasks)
+
+# Ruta para agregar una nueva tarea
+@app.route('/add_task', methods=['POST'])
+def add_task():
+    task_content = request.form['content']
+    tasks.append(task_content)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
